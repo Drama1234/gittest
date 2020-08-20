ad351ebf29fbbb7c66bb061b88653cf747d789e3
drama <tianzhuojing@foxmail.com>
2020-08-20 13:24:16 +0800

    git test init

---
+++ main/java/com/kaikeba/gittest/GitDemo.java
@@ -0,0 +1,12 @@
+package com.kaikeba.gittest;
+
+/**
+ * @author drama
+ * @date 2020/8/20
+ * @project mavenDemo01
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
---
+++ main/java/com/kaikeba/hello1.java
@@ -0,0 +1,13 @@
+package com.kaikeba;
+
+/**
+ * @author drama
+ * @date 2020/8/19
+ * @project mavenDemo01
+ */
+public class hello1 {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+
+}
