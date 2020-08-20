b8d48f300dde87f694c6623f750d80042fffedb2
drama <tianzhuojing@foxmail.com>
2020-08-20 13:36:09 +0800

    update gittest

--- main/java/com/kaikeba/gittest/GitDemo.java
+++ main/java/com/kaikeba/gittest/GitDemo.java
@@ -8,5 +8,6 @@ package com.kaikeba.gittest;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 }
